48fbf1aee805ec8c2ee7dd64b9e48025bc5019ee
Gilmour <horriganb@gilmour.org>
2015-08-31 15:11:04 -0400

    Started mapping

---
+++ src/ttt.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ttt {
+	public static void main(String[] args){
+		//variables and a welcome message 
+		//game loop
+		int var1 = 1;
+		System.out.println("Let's Get Ready To Rumble!");
+		System.out.println("[var1][2][3]");
+		System.out.println("[4][5][6]");
+			//switch players
+	}
+}
